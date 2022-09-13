bb1a6df4a7617ef3208fe2eade5811200f276019
Fabio Arrivabene <107645610+FArrivabene@users.noreply.github.com>
2022-09-13 16:09:17 +0100

    Create add-to-cart-new.js

---
+++ add-to-cart-new.js
@@ -0,0 +1,25 @@
+<script>
+    //JS ADD TO CART
+    function addToCart(supermodelId, skuId) {
+      cube.api.addToCart(
+        {
+          supermodelId: supermodelId,
+          skuId: skuId,
+          quantity: 1,
+          analytics: {}
+        }
+      ).then(response => { console.log(modal_container.classList.add("show"))
+    });
+    }
+  
+    var addToCartButtons = document.querySelectorAll('.add-to-cart')
+    addToCartButtons.forEach((button) => {
+      button.addEventListener('click', (evn) => {
+        evn.preventDefault();
+        var model = button.getAttribute('data-model');
+        var sku = button.getAttribute('data-sku');
+        addToCart(model, sku);
+        console.log('model/sku', model + '/' + sku);
+      })
+    })
+  </script>
